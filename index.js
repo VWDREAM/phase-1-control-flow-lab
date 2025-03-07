45c07c250aa7f8af88d113df672ead88cf24a803
VWDREAM <wanjiruvirginiah26@gmail.com>
2025-03-07 12:11:06 +0300

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if(feet <= 400){
+    return 'This one is on me!'
+  }
+  else if
+    (feet > 400 && feet <= 2000){
+      return 'That will be twenty bucks.'
+  }
+  else if( feet >2000 && feet<=2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+else{
+  return 'No can do.'
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cities){
   // Write your code here!
+  return cities === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tips){
   // Write your code here!
+  return tips === "generous"? "Thank you so much." : tips ==="not as generous"? "Thank you.":"Bye.";
+
 }
